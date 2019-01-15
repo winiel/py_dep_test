c693bf6a2c24c02618d124c954890df61c96ace0
SohnYongJin <winiel@smfcast.com>
2019-01-15 23:06:48 +0900

    init

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
 
 import time;
 
@@ -11,6 +14,8 @@ today = datetime.now().isoformat();
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
 
 driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=options);
 
@@ -21,10 +26,24 @@ driver.find_element_by_css_selector("input[placeholder=ID]").send_keys("pylun");
 driver.find_element_by_css_selector("input[placeholder=Password]").send_keys("password");
 driver.find_element_by_css_selector("button[type=submit]").click();
 time.sleep(1);
+print(driver.page_source);
+print("=====================")
+print("=====================")
+print("=====================")
 
 url = "http://sandbox.www.pylun.com/config/market";
 driver.get(url);
-driver.find_element_by_xpath("//button[contains(text(), '등록')]").click();
+print(driver.page_source);
+print("=====================")
+print("=====================")
+print("=====================")
+
+time.sleep(2);
+btn = driver.find_element_by_css_selector("button[ng-click='btnReg()']");
+# btn = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(), '등록')]']")));
+# btn = WebDriverWait(driver, 3).until((EC.presence_of_element_located, "button[ng-click='btnReg()']"));
+btn.click();
 time.sleep(1);
 driver.find_element_by_css_selector("input[placeholder='마켓 이름을 입력해주세요.']").send_keys(today);
-driver.find_element_by_xpath("//button[contains(text(), '저장')]").click();
+driver.find_element_by_css_selector("button[ng-click='btnSave()']").click();
+print("end");
